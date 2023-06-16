5e5b9dbaad81ed5ecfad2b1818fad5f8a49e0cd8
NouraNou <oneway.33@hotmail.com>
2023-06-16 14:57:32 +0300

    iteration methods

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const numbers = [1, 2, 3, 4, 5];
 // write code to create a new array where each
 // number is multiplied by 2 using the map method.
 // ============== Your answer here:
-
+const double = numbers.map(function (number) {
+  return number * 2;
+});
+console.log(double);
 // ==============
 
 // Q2)
@@ -13,7 +16,10 @@ const names = ["Alice", "Bob", "Charlie"];
 // write code to create a new array where each name is
 // converted to uppercase using the map method.
 // ============== Your answer here:
-
+const uppercase = names.map(function (name) {
+  return name.toUpperCase;
+});
+console.log(uppercase);
 // ==============
 
 // Q3)
@@ -22,7 +28,10 @@ const numbers2 = [1, 4, 9, 16, 25];
 // write code to create a new array where each
 //  number is squared using the map method.
 // ============== Your answer here:
-
+const squared = numbers2.map(function (number2) {
+  return number2 * number2;
+});
+console.log(squared);
 // ==============
 
 // Q4)
@@ -31,7 +40,10 @@ const names2 = ["John", "Jane", "Tom"];
 // write code to create a new array where each name
 // is appended with " Smith" using the map method.
 // ============== Your answer here:
-
+const newnames2 = names2.map(function (name) {
+  return name + "Smith";
+});
+console.log(newnames2);
 // ==============
 
 // Q5)
@@ -40,7 +52,10 @@ const numbers3 = [2, 4, 6, 8, 10];
 //  write code to create a new array where each
 // number is divided by 2 using the map method.
 // ============== Your answer here:
-
+const division = numbers3.map(function (number) {
+  return number / 2;
+});
+console.log(division);
 // ==============
 
 // Q6)
@@ -49,5 +64,12 @@ const fruits = ["apple", "banana", "orange"];
 //  write code to create a new array where each fruit
 //  name is capitalized using the map method. Ex, Apple ...
 // ============== Your answer here:
+function capitalizeWords(fruits) {
+  return fruits.map((word) => {
+    const firstLetter = word.charAt(0).toUpperCase();
+    const rest = word.slice(1).toLowerCase();
 
+    return firstLetter + rest;
+  });
+}
 // ==============
